dcc2598e1144c9e6c77aadf1e74b032aabf6ea9d
sarita595k <sarita595k@gmail.com>
2026-01-03 19:49:26 +0530

    navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './Components/Navbar';
+import Container from 'react-bootstrap/esm/Container';
 // import { Button } from 'react-bootstrap';
 const App = () => {
   return (<>
     <NavbarComponent />
-    <h1>Avsar social</h1>
+    <Container className='text-center'>
+      <h1 className='display-3'>AvsarSocialMedia</h1>
+      <p className='lead'>Find Friends and share your thoughts...........💭</p>
+    </Container>
     {/* <Button variant="primary">Primary</Button> */}
   </>)
 }
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -20,25 +20,14 @@ const NavbarComponent = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#features">Posts</Nav.Link>
+                        <Nav.Link href="#features">Followers</Nav.Link>
+                        <Nav.Link href="#pricing">Following</Nav.Link>
+                        <Nav.Link href="#pricing">Suggestions</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        <Nav.Link href="#deets">Sign up</Nav.Link>
+                        <Nav.Link href="#deets">Sign in</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
